9e36701e65c88d89ed0ce95af9d857647e90e628
Kampukter <a.v.zadvernyak@gmail.com>
2021-09-05 16:49:14 +0300

    05/09/2021 daily commit

--- app/src/main/java/m/kampukter/smarthomemanagement/ui/remotedata/SensorRemoteListViewHolder.kt
+++ app/src/main/java/m/kampukter/smarthomemanagement/ui/remotedata/SensorRemoteListViewHolder.kt
@@ -2,6 +2,8 @@ package m.kampukter.smarthomemanagement.ui.remotedata
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import m.kampukter.smarthomemanagement.R
+import m.kampukter.smarthomemanagement.data.DeviceType
 import m.kampukter.smarthomemanagement.data.SensorInfoRemote
 import m.kampukter.smarthomemanagement.databinding.SensorRemoteItemBinding
 import m.kampukter.smarthomemanagement.ui.ClickEventDelegate
@@ -15,13 +17,31 @@ class SensorRemoteListViewHolder(
 
         with(sensorRemoteItemView) {
             sensorRemoteNameTextView.text = result.name
-            sensorRemoteDescriptionTextView.text = result.measure
-            if (result.isCandidate) {
-                sensorRemoteItemView.root.setOnClickListener {
-                    clickUnitEventDelegate.onClick(result)
+            sensorRemoteMeasureTextView.text = sensorRemoteItemView.root.context.getString(
+                R.string.sensor_measure, result.measure
+            )
+            when (result.deviceType) {
+                DeviceType.RELAY -> {
+                    sensorRemoteTypeTextView.text = "Тип: Реле"
+                    sensorRemoteMeasureTextView.visibility = View.INVISIBLE
                 }
+                DeviceType.Device -> {
+                    sensorRemoteTypeTextView.text = "Тип: Измеритель"
+                    sensorRemoteMeasureTextView.visibility = View.VISIBLE
+
+                }
+            }
+
+            if (result.isCandidate) {
+                sensorRemoteItemView.root.setOnClickListener { clickUnitEventDelegate.onClick(result) }
                 addSensorImageView.visibility = View.VISIBLE
-            } else addSensorImageView.visibility = View.INVISIBLE
+                checkSensorImageView.visibility = View.INVISIBLE
+            } else {
+                addSensorImageView.visibility = View.INVISIBLE
+                checkSensorImageView.visibility = View.VISIBLE
+            }
+
+
         }
 
     }
